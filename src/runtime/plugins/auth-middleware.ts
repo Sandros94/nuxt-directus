501f22f3e82f34e6349d582ff3e7ce84cd44587c
Sandros94 <sandro.circi@digitoolmedia.com>
2024-01-31 02:02:10 +0100

    feat: optional auth middleware with redirect

---
+++ src/runtime/plugins/auth-middleware.ts
@@ -0,0 +1,29 @@
+import { useDirectusUsers } from '../composables/use-directus-users'
+import {
+  addRouteMiddleware,
+  defineNuxtPlugin,
+  navigateTo,
+  onNuxtReady,
+  useRuntimeConfig
+} from '#imports'
+
+export default defineNuxtPlugin((_nuxtApp) => {
+  const {
+    global,
+    name,
+    redirect,
+    to: toArray
+  } = useRuntimeConfig().public.directus.moduleConfig.authMiddleware
+
+  addRouteMiddleware(name, (to, _from) => {
+    const { user } = useDirectusUsers()
+
+    onNuxtReady(() => {
+      if (!user.value && (toArray.length === 0 || !toArray.indexOf(to.path))) {
+        return navigateTo(redirect)
+      }
+    })
+  }, {
+    global
+  })
+})
